c70c898f622d2538b7bc1060163cc9f38adf1f2b
AliciaSun08 <60821573+AliciaSun08@users.noreply.github.com>
2020-02-16 10:20:45 -0800

    Create 110.java

---
+++ src/1-500/107/110.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(checkHeight(root) == -1){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    
+    public static int checkHeight(TreeNode root){
+        if(root == null) return 0;
+        int leftHeight = checkHeight(root.left);
+        if(leftHeight == -1) return -1;
+        
+        int rightHeight = checkHeight(root.right);
+        if(rightHeight == -1) return -1;     
+        
+        int heightDiff = leftHeight - rightHeight;
+        if(Math.abs(heightDiff) > 1){
+            return -1;
+        }
+        else{
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+}
