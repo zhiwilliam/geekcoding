378ff9d5ee3e033d79e99f757e532467965d0ccc
zhiwi <zhiwilliam@gmail.com>
2020-02-18 10:12:25 -0500

    Question 130

---
+++ src/1-500/131/paliadromePartition.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        if not s: return
+        l = len(s)
+        dp, rst = [[0] * l for _ in range(l)], [[] for _ in range(l + 1)]
+        rst[0].append([])
+        for j in range(l):
+            for i in range(j + 1):
+                if s[i] == s[j] and (j - i <= 2 or dp[i + 1][j - 1]):
+                    dp[i][j] = 1
+                    apal = s[i:j + 1]
+                    for lst in rst[i]:
+                        rst[j + 1].append(lst + [apal])
+        return rst
+
+solution = Solution()
+print(solution.partition("aab"))
