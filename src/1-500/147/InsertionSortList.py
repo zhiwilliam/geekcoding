7c2e4823fabfb43aa05ffefa7c4f07ddd6ce3e46
zhiwi <zhiwilliam@gmail.com>
2020-02-20 11:45:09 -0500

    Question 147

---
+++ src/1-500/147/InsertionSortList.py
@@ -0,0 +1,20 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def insertionSortList(self, head: ListNode) -> ListNode:
+        root = head
+        while head.next:
+            if head.val <= head.next.val:
+                head = head.next
+            else:
+                temp = head.next
+                q = root
+                head.next = head.next.next
+                while q.next and q.next.val < temp.val:
+                    q = q.next
+                temp.next = q.next
+                q.next = temp
+        return root.next
---
+++ src/1-500/147/README.md
@@ -0,0 +1,14 @@
+# 题目
+https://leetcode.com/problems/insertion-sort-list/
+
+# 级别 
+Medium
+
+# 算法口号
+放进数组做
+
+# 解题思路
+这道题用链表做的话居然不过。我看能接受的方案都是放进数组做。但放进数组基本上是毫无意义的题目。
+
+# 算法归类
+<a href="../../../TE.md">特殊题</a>
