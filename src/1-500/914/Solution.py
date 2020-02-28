5dafbad23801cb7017ebfadc1831e4594c45c88b
Steve Li <lichengzhi75@hotmail.com>
2020-02-28 14:09:12 -0500

    modify Python version

--- src/1-500/914/Solution.py
+++ src/1-500/914/Solution.py
@@ -3,7 +3,7 @@ from typing import List
 class Solution:
     def hasGroupsSizeX(self, deck: List[int]) -> bool:
         from collections import Counter
-        # from functools import reduce
+        from functools import reduce
         vals = Counter(deck).values()
         return reduce(self.gcd, vals) >= 2
 
