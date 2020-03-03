7bbf3f5a147e028e19929e911b9562527f3ec898
Chris Zhang <zrzllyz@outlook.com>
2020-03-02 21:15:50 -0500

    Leetcode 1057 add count sort solution

--- src/1001-1500/1057/CampusBikes.java
+++ src/1001-1500/1057/CampusBikes.java
@@ -32,3 +32,43 @@ class Solution {
         return res;
     }
 }
+
+/**
+ * The count sort solution is brilliantly.
+ * Can just store all the pairs based on their distance
+ * Just copied and paste from discussion
+ */
+class Solution {
+    public int[] assignBikes(int[][] workers, int[][] bikes) {
+        // Notice that the Manhattan distance is between 0 and 2000,
+        // which means we can sort easily without even using priority queue
+        int w = workers.length, b = bikes.length;
+        int[] wo = new int[w], bi = new int[b];
+        List<int[]>[] dists = new List[2001];
+        Arrays.fill(wo, -1);
+        Arrays.fill(bi, -1);
+        for (int i = 0; i < w; i++) {
+            for (int j = 0; j < b; j++) {
+                int[] worker = workers[i];
+                int[] bike = bikes[j];
+                int dist = Math.abs(worker[0] - bike[0]) + Math.abs(worker[1] - bike[1]);
+                if (dists[dist] == null) {
+                    dists[dist] = new ArrayList<int[]>();
+                }
+                dists[dist].add(new int[]{i, j});
+            }
+        }
+        int assigned = 0;
+        for (int i = 0; i <= 2000 && assigned < w; i++) {
+            if (dists[i] == null) continue;
+            for (int[] pair : dists[i]) {
+                if (wo[pair[0]] == -1 && bi[pair[1]] == -1) {
+                    wo[pair[0]] = pair[1];
+                    bi[pair[1]] = pair[0];
+                    assigned++;
+                }
+            }
+        }
+        return wo;
+    }
+}
