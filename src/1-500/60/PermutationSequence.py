330dc6e50d2cbdf4c03e250807ec21502085b6d5
zhiwi <zhiwilliam@gmail.com>
2020-03-03 16:04:12 -0500

    Question 60

---
+++ src/1-500/60/PermutationSequence.py
@@ -0,0 +1,18 @@
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        ans = ''
+        fact = [1] * n
+        num = [str(i) for i in range(1, 10)]
+        for i in range(1, n):
+            fact[i] = fact[i - 1] * i
+        k -= 1
+        for i in range(n, 0, -1):
+            first = k // fact[i - 1]
+            k %= fact[i - 1]
+            ans += num[first]
+            num.pop(first)
+        return ans
+
+
+solution = Solution()
+print(solution.getPermutation(3, 5))
