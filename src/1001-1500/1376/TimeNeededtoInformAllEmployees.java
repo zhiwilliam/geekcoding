2ad5632cd7d799d3da013b235bd71ba022187726
Chris Zhang <zrzllyz@outlook.com>
2020-03-08 12:16:54 -0400

    leetcode 1376 add bottom up recursion

--- src/1001-1500/1376/TimeNeededtoInformAllEmployees.java
+++ src/1001-1500/1376/TimeNeededtoInformAllEmployees.java
@@ -1,4 +1,5 @@
 class Solution {
+    //we can have another bottom-up recuresion to do this
     //build a tree using map
     private int ans = 0;
     public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
@@ -41,5 +42,30 @@ class Node {
 
     public Node(int id) {
         this.sub = new ArrayList<>();
+    }
+}
+
+//Bottom-up recuresion
+
+class Solution {
+    public int numOfMinutes(final int n, final int headID, final int[] manager, final int[] informTime) {
+        final Map<Integer, List<Integer>> graph = new HashMap<>();
+        int total = 0;
+        for (int i = 0; i < manager.length; i++) {
+            int j = manager[i];
+            if (!graph.containsKey(j))
+                graph.put(j, new ArrayList<>());
+            graph.get(j).add(i);
+        }
+        return dfs(graph, informTime, headID);
+    }
+
+    private int dfs(final Map<Integer, List<Integer>> graph, final int[] informTime, final int cur) {
+        int max = 0;
+        if (!graph.containsKey(cur))
+            return max;
+        for (int i = 0; i < graph.get(cur).size(); i++)
+            max = Math.max(max, dfs(graph, informTime, graph.get(cur).get(i)));
+        return max + informTime[cur];
     }
 }
