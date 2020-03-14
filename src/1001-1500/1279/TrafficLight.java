7303fceffc2c07579ee030b63126ac691a2f3afa
Chris Zhang <zrzllyz@outlook.com>
2020-03-14 17:48:43 -0400

    leetcode 1279

---
+++ src/1001-1500/1279/TrafficLight.java
@@ -0,0 +1,29 @@
+class TrafficLight {
+    private final Signal signal;
+    public TrafficLight() {
+        this.signal = new Signal();
+    }
+
+    public void carArrived(
+            int carId,           // ID of the car
+            int roadId,          // ID of the road the car travels on. Can be 1 (road A) or 2 (road B)
+            int direction,       // Direction of the car
+            Runnable turnGreen,  // Use turnGreen.run() to turn light to green on current road
+            Runnable crossCar    // Use crossCar.run() to make car cross the intersection
+    ) {
+        synchronized(signal) {
+            if (signal.curSignal != roadId) {
+                turnGreen.run();
+                signal.curSignal = roadId;
+            }
+            crossCar.run();
+        }
+    }
+}
+
+class Signal {
+    int curSignal; // curSignal == roadId means green on that road
+    public Signal() {
+        this.curSignal = 1;
+    }
+}
