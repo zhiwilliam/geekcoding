b07efad07db4a3a4397c480df86b89baba6653d0
Jervis <jervis.work@gmail.com>
2020-03-16 22:40:17 -0400

    Adding problem 14

---
+++ src/1-500/014/Longest Common Prefix.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        size = len(strs)
+        if (size == 0):
+            return ""
+        if (size == 1):
+            return strs[0]
+        strs.sort()
+        print(strs)
+        end = min([len(s) for s in strs])
+        i = 0
+        while i < end and strs[0][i] == strs[size - 1][i]:
+            i += 1
+        return strs[0][0: i] 
