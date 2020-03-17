f29a37f09e59668d51e501d734865392aae319be
Eddie Song <eddie.y.song@gmail.com>
2020-03-16 20:52:38 -0400

    Problem 344, additional one-liners

--- src/1-500/344/reverse_string.py
+++ src/1-500/344/reverse_string.py
@@ -6,3 +6,10 @@ class Solution:
         for i in range(0, len(s) // 2):
             s[i], s[len(s) - 1 - i] = s[len(s) - 1 - i], s[i]
             
+
+        """
+        One-liners, probably unsuitable for interviews
+        """
+        # s.reverse()
+
+        # s[:] = s[::-1]
