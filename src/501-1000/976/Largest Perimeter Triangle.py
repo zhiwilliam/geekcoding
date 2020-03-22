57639df413de4d82e0d9ae0a4aede0519d3886c5
fish117 <32891716+fish117@users.noreply.github.com>
2020-03-22 12:35:21 -0400

    Create Largest Perimeter Triangle.py

---
+++ src/501-1000/976/Largest Perimeter Triangle.py
@@ -0,0 +1,8 @@
+    def largestPerimeter(self, A):
+        A = sorted(A)[::-1]
+        for i in range(len(A) - 2):
+            if A[i] < A[i + 1] + A[i + 2]:
+                return A[i] + A[i + 1] + A[i + 2]
+        return 0
+        
+   
