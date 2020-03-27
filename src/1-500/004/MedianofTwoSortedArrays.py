370f80311827d4fd4f2cf55cff9cd1397099bc8d
zhiwi <zhiwilliam@gmail.com>
2020-03-26 21:25:03 -0400

    question 4

--- src/1-500/004/MedianofTwoSortedArrays.py
+++ src/1-500/004/MedianofTwoSortedArrays.py
@@ -3,37 +3,44 @@ from typing import List
 
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        index1 = 0
-        index2 = 0
-        mergedArr = []
-        while index1 < len(nums1) and index2 < len(nums2):
-            if nums1[index1] > nums2[index2]:
-                mergedArr.append(nums2[index2])
-                index2 += 1
-            elif nums1[index1] < nums2[index2]:
-                mergedArr.append(nums1[index1])
-                index1 += 1
-            else:  # equal
-                mergedArr.append(nums1[index1])
-                mergedArr.append(nums2[index2])
-                index1 += 1
-                index2 += 1
+        m, n = len(nums1), len(nums2)
+        if m > n:
+            nums1, nums2, m, n = nums2, nums1, n, m
+        if n == 0:
+            raise ValueError
 
-        if index1 != len(nums1):
-            while index1 < len(nums1):
-                mergedArr.append(nums1[index1])
-                index1 += 1
+        imin, imax, half_len = 0, m, (m + n + 1) // 2
+        while imin <= imax:
+            i = (imin + imax) // 2
+            j = half_len - i
+            if i < m and nums2[j - 1] > nums1[i]:
+                # i is too small, must increase it
+                imin = i + 1
+            elif i > 0 and nums1[i - 1] > nums2[j]:
+                # i is too big, must decrease it
+                imax = i - 1
+            else:
+                # i is perfect
 
-        if index2 != len(nums2):
-            while index2 < len(nums2):
-                mergedArr.append(nums2[index2])
-                index2 += 1
+                if i == 0:
+                    max_of_left = nums2[j - 1]
+                elif j == 0:
+                    max_of_left = nums1[i - 1]
+                else:
+                    max_of_left = max(nums1[i - 1], nums2[j - 1])
 
-        mergedLen = len(mergedArr)
-        if mergedLen % 2 == 0:  # even length
-            return float(mergedArr[mergedLen // 2] + mergedArr[mergedLen // 2 - 1]) / 2
-        else:
-            return float(mergedArr[(mergedLen - 1) // 2])
+                if (m + n) % 2 == 1:
+                    return max_of_left
+
+                if i == m:
+                    min_of_right = nums2[j]
+                elif j == n:
+                    min_of_right = nums1[i]
+                else:
+                    min_of_right = min(nums1[i], nums2[j])
+
+                return (max_of_left + min_of_right) / 2.0
+
 
 solution = Solution()
-print(solution.findMedianSortedArrays([1, 3], [2]))
+print(solution.findMedianSortedArrays([1, 3, 5, 6], [8]))
--- src/1-500/004/README.md
+++ src/1-500/004/README.md
@@ -1,4 +1,5 @@
-题目
+# 题目
+## Median of Two Sorted Arrays
 There are two sorted arrays nums1 and nums2 of size m and n respectively.
 
 Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
@@ -18,5 +19,16 @@ nums2 = [3, 4]
 
 The median is (2 + 3)/2 = 2.5
 
-算法归类
-Binary search
+# 算法口号
+二分查找，双指针算index
+
+# 解题思路
+这道题一定要画图分析
+
+它的思路就是分别取两个数组的中点，然后比大小。注意这两句很重要<br>
+i = (imin + imax) // 2 实现了折半查找<br>
+j = half_len - i       这句是在i移动的时候j跟着相对移动。
+通过修改imin和imax，它实现了两个数组中同时折半移动。
+最后i is perfect的时候的处理分了总长度是奇数个还是偶数个两种情况。
+
+这道题比较复杂，简单几句话很难讲清。建议大家运行几遍体会。不简单。
