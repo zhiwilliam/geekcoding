4fadcde3f0682fbc9130ce18ee02e8739696eaa1
Eric <erichuan2020@gmail.com>
2020-04-16 23:02:11 -0400

    LC 819

---
+++ src/501-1000/819/MostCommonWord.java
@@ -0,0 +1,33 @@
+package OA;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class MostCommonWord {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        Set<String> set = new HashSet<>(Arrays.asList(banned));
+        
+        String[] para = paragraph.toLowerCase().split("\\W+");
+        //String[] words = paragraph.toLowerCase().split("[ !?',;.]+");
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(String str : para){
+            if(!set.contains(str))
+                map.put(str, map.getOrDefault(str, 0) + 1);
+        }
+        
+        int max = 0;
+        String tem = null;
+        for(Map.Entry<String, Integer> entry : map.entrySet()){
+            if(entry.getValue() > max){
+                max = entry.getValue();
+                tem = entry.getKey();
+            }
+        }
+        
+        return tem;
+    }
+}
